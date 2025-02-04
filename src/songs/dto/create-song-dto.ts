b7f83bea8db4c37d7bc00c8e215d49fbb0a66da4
Guido Castrogiovanni <nicocastrogiovanni@hotmail.com>
2025-02-04 00:23:29 -0300

    Fix missing attribute. @IsString validation for each in @IsArray

--- src/songs/dto/create-song-dto.ts
+++ src/songs/dto/create-song-dto.ts
@@ -1,21 +1,26 @@
-import { IsArray, IsDateString, IsMilitaryTime, IsNotEmpty, IsString } from "class-validator";
+import {
+   IsArray,
+   IsDateString,
+   IsMilitaryTime,
+   IsNotEmpty,
+   IsString,
+} from 'class-validator';
 
 export class CreateSongDTO {
+   @IsNotEmpty()
+   @IsString()
+   readonly title: string;
 
-    @IsNotEmpty()
-    @IsString()
-    readonly title : string;
+   @IsNotEmpty()
+   @IsArray()
+   @IsString({ each: true })
+   readonly artists: string[];
 
-    @IsNotEmpty()
-    @IsArray()
-    readonly artists : string[];
+   @IsNotEmpty()
+   @IsDateString()
+   readonly releaseDate: Date;
 
-    @IsNotEmpty()
-    @IsDateString()
-    readonly releaseDate : Date;
-
-    @IsNotEmpty()
-    @IsMilitaryTime()
-    readonly duration : Date;
-
+   @IsNotEmpty()
+   @IsMilitaryTime()
+   readonly duration: Date;
 }
